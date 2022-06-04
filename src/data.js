8377fb941960982e7431bd5c7ea514a27a53296d
Carlos Hermosillo <hermcode@gmail.com>
2022-06-04 13:03:33 -0500

    [modified] Data updated

--- src/data.js
+++ src/data.js
@@ -6,40 +6,49 @@ export const data = [
     date: '2 h',
     description: '¡¡EL MINISTRO DE DEFENSA Y ATAQUE!!🚀 Checo Pérez🇲🇽 tiene la razón más alta entre el número de adelantamientos y las veces que fue superado (11/1), en la actual temporada de F1🔥. Tremenda estadística',
     postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
-    likes: {
-      like: 20,
-      love: 80,
+    reactions: {
+      like: 150,
+      love: 81,
       fun: 120
     },
-    comments: 50
+    comments: [
+      { user: 'Carlos Hermosillo', message: 'Vamos Checoooo'},
+      { user: 'Luis Ramos', message: 'Eres el mejor!'},
+      { user: 'Daniel Vega', message: 'Never Give Up'},
+    ]
   },
   {
     id: 2,
     username: 'Velocidad al limite',
     pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t1.6435-9/61884291_487299918477849_6430058434342682624_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2GcZu9aZi5EAX9xRVKC&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8eR6bYi8lge8O-i7jQ-PHBm1Q522EbvFgam7OFOsPnkA&oe=62BEB506',
     date: '2 h',
-    description: 'Ay mi madre el bicho',
-    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
-    likes: {
+    description: `¡¡DOS SERIOS CANDIDATOS AL TÍTULO!!❤️💙
+    “No importa quien de los dos sea campeón de la F1. Por supuesto, el campeonato de constructores es muy importante para nosotros . Pero tanto sea Max o Checo, ambos son pilotos de Red Bull, y tienen las mismas oportunidades”.
+    Christian Horner`,
+    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285671263_1214807722393728_2880891571569900902_n.jpg?stp=dst-jpg_p526x296&_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=M6QIb4AAo5sAX_8ngNv&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8up_aKS9xaoXUCyhR_uduWWyjiHHkT8YP4DCmsFDUF6A&oe=62A02F87',
+    reactions: {
       like: 20,
       love: 80,
       fun: 120
     },
-    comments: 50
+    comments: [
+      { user: 'hermcode', message: 'Vamos Checoooo'},
+      { user: 'hermcode', message: '¡¡EL MINISTRO DE DEFENSA Y ATAQUE!!🚀 Checo Pérez🇲🇽 tiene la razón más alta entre el número de adelantamientos y las veces que fue superado (11/1), en la actual temporada de F1🔥. Tremenda estadística'},
+    ]
   },
   {
     id: 3,
-    username: 'Velocidad al limite',
-    pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t1.6435-9/61884291_487299918477849_6430058434342682624_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2GcZu9aZi5EAX9xRVKC&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8eR6bYi8lge8O-i7jQ-PHBm1Q522EbvFgam7OFOsPnkA&oe=62BEB506',
-    date: '2 h',
-    description: '¡¡EL MINISTRO DE DEFENSA Y ATAQUE!!🚀 Checo Pérez🇲🇽 tiene la razón más alta entre el número de adelantamientos y las veces que fue superado (11/1), en la actual temporada de F1🔥. Tremenda estadística',
-    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
-    likes: {
+    username: 'Oracle Red Bull Racing',
+    pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/273222220_4923546217691828_1130765426768417188_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Nut2oNPmVDYAX-ooqK6&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8BqaJohvMxe7MkG6wbkikVaI6qQ09QZZjHUFJDLJQSTQ&oe=62A163E1',
+    date: '1 de junio a a las 10:30',
+    description: '',
+    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/284137265_5237467679633012_4272397185029982591_n.jpg?stp=dst-jpg_p180x540&_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_ohc=yA-wg9xFic0AX_iu1_o&_nc_ht=scontent.fntr5-1.fna&oh=00_AT_Lk-9ZY81MHawpIQ-VhWcV1ZAaz528HC5vb4KIw5RsbQ&oe=629F96E9',
+    reactions: {
       like: 20,
-      love: 80,
-      fun: 120
+      love: 61,
+      fun: 10
     },
-    comments: 50
+    comments: []
   },
   
 ]
