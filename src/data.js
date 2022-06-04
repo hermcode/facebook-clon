49ec03acfae6407933a6a51b505a6c4d6e42075d
Carlos Hermosillo <hermcode@gmail.com>
2022-06-04 09:10:06 -0500

    [added] First input data

---
+++ src/data.js
@@ -0,0 +1,45 @@
+export const data = [
+  {
+    id: 1,
+    username: 'Velocidad al Limite',
+    pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t1.6435-9/61884291_487299918477849_6430058434342682624_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2GcZu9aZi5EAX9xRVKC&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8eR6bYi8lge8O-i7jQ-PHBm1Q522EbvFgam7OFOsPnkA&oe=62BEB506',
+    date: '2 h',
+    description: '¡¡EL MINISTRO DE DEFENSA Y ATAQUE!!🚀 Checo Pérez🇲🇽 tiene la razón más alta entre el número de adelantamientos y las veces que fue superado (11/1), en la actual temporada de F1🔥. Tremenda estadística',
+    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
+    likes: {
+      like: 20,
+      love: 80,
+      fun: 120
+    },
+    comments: 50
+  },
+  {
+    id: 2,
+    username: 'Velocidad al limite',
+    pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t1.6435-9/61884291_487299918477849_6430058434342682624_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2GcZu9aZi5EAX9xRVKC&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8eR6bYi8lge8O-i7jQ-PHBm1Q522EbvFgam7OFOsPnkA&oe=62BEB506',
+    date: '2 h',
+    description: 'Ay mi madre el bicho',
+    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
+    likes: {
+      like: 20,
+      love: 80,
+      fun: 120
+    },
+    comments: 50
+  },
+  {
+    id: 3,
+    username: 'Velocidad al limite',
+    pp: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t1.6435-9/61884291_487299918477849_6430058434342682624_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=2GcZu9aZi5EAX9xRVKC&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8eR6bYi8lge8O-i7jQ-PHBm1Q522EbvFgam7OFOsPnkA&oe=62BEB506',
+    date: '2 h',
+    description: '¡¡EL MINISTRO DE DEFENSA Y ATAQUE!!🚀 Checo Pérez🇲🇽 tiene la razón más alta entre el número de adelantamientos y las veces que fue superado (11/1), en la actual temporada de F1🔥. Tremenda estadística',
+    postImage: 'https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46',
+    likes: {
+      like: 20,
+      love: 80,
+      fun: 120
+    },
+    comments: 50
+  },
+  
+]
