e19e841f69913b227a63a5ce6f35dd7abc815afe
Carlos Hermosillo <hermcode@gmail.com>
2022-06-04 13:09:36 -0500

    [modified] Styles

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CardHeader from './CardHeader';
 import Description from './Description';
+import Reactions from './Reactions';
 
 import '../styles/Card.css'
+import CommentsSection from './CommentsSection';
+import CardButtons from './CardButtons';
 
 const Card = ({post}) => {
 
-  
+  const [ comments, setComments] = useState(post.comments)
+  const [showComments, setShowComments] = useState(false)
+
+  const handleShowComments = () => setShowComments(!showComments)
+
+  useEffect(() => {
+    setComments(post.comments)
+  }, [post.comments])
 
   return (
     <div className='card'>
@@ -15,21 +25,27 @@ const Card = ({post}) => {
       <Description description={post.description}/>
 
       <figure>
-        <img src='https://scontent.fntr5-1.fna.fbcdn.net/v/t39.30808-6/285623717_1214979345709899_6655235087880127644_n.jpg?stp=dst-jpg_p526x296&_nc_cat=105&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=cfcDjB880mcAX8PNxlh&_nc_ht=scontent.fntr5-1.fna&oh=00_AT8HQD1AvMJWpx1VjdWzEhhn6hOOjyaKgmsMIdnTKCwjnw&oe=629F8B46' alt="" />
+        <img src={post.postImage} alt="Post image" />
       </figure>
 
-      <div>
-        <div>
-          <span>😂</span>
-          <span>❤️</span>
-          <span>👍</span>
-          <p>172</p>
-        </div>
+      <div className='reactions-data'>
+        <Reactions reactions={post.reactions}/>
 
-        <div>
-
+        <div className='comments-counter' onClick={handleShowComments}>
+          {
+            comments.length > 0 && <p>{comments.length} { 
+              comments.length === 1 ? 'comentario' : 'comentarios' 
+            }</p>
+          }
         </div>
       </div>
+      <hr className='card-hr'/>
+      <CardButtons handleShowComments={handleShowComments}/>
+
+      {
+        showComments && <CommentsSection comments={comments} setComments={setComments}/>
+      }
+      
     </div>
   )
 }
